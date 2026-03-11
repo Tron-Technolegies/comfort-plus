65277f71c2cf35ba436e722f6096a6e5128a8f9d
savithnarayan111 <savithnarayan111@gmail.com>
2026-03-11 13:40:53 +0530

    Added user signup view

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from UserApp.models import  Profile
+
+
+
+
 
 # Create your views here.
+
+@csrf_exempt
+def user_signup(request):
+    if request.method=='POST':
+        rname=request.POST.get("name")
+        rmail=request.POST.get("mail")
+        rphone=request.POST.get("phone")
+        rpass=request.POST.get("pass")
+        if not rmail or not rphone:
+            return HttpResponse("This field is mandatory")
+        user=User.objects.create_user(
+            username=rname,
+            email=rmail, 
+            password=rpass
+        )
+        Profile.objects.create(
+            user=user,
+            Phone=rphone
+        )
+    return JsonResponse({'message':'successfully'})
