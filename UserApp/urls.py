3112538e11888110db42f3a7796d2863b21d64b8
savithnarayan111 <savithnarayan111@gmail.com>
2026-03-12 12:19:22 +0530

    updated profile API

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -4,4 +4,8 @@ from UserApp import views
 urlpatterns = [
     
     path("signup/", views.user_signup, name="user_signup"),
+
+    path("view_profile",views.view_profile,name="view_profile"),
+    path("update_profile",views.update_profile,name="update_profile"),
+    path("delete_profile",views.delete_profile,name="delete_profile"),
 ]
--- UserApp/views.py
+++ UserApp/views.py
@@ -15,6 +15,12 @@ def user_signup(request):
         rpass=request.POST.get("pass")
         if not rmail or not rphone:
             return HttpResponse("This field is mandatory")
+        
+
+        if User.objects.filter(email=rmail).exists():
+           return HttpResponse("Email already exists")
+        
+
         user=User.objects.create_user(
             username=rname,
             email=rmail, 
@@ -24,4 +30,55 @@ def user_signup(request):
             user=user,
             Phone=rphone
         )
-    return JsonResponse({'message':'successfully'})
+        return JsonResponse({'message': 'successfully created'})
+
+    return JsonResponse({'message': 'invalid request'})
+
+
+#*****************************PROFILE*******************************
+
+#VIEW PROFILE
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@csrf_exempt
+def view_profile(request):
+    user=request.user
+    profile=user.profile
+    phone = profile.phone
+
+    data={
+        "name":user.username,
+        "mail":user.email,
+        "phone":phone
+    }
+
+    return JsonResponse(data)
+
+#UPDATE PROFILE
+
+@api_view(['POST'])
+@permisiion_classes([IsAuthenticated])
+def update_profile(request):
+    user=request.user
+    profile=user.profile
+    user.username=request.data.get("name",user.username)
+    user.save()
+    new_email = request.data.get("email", user.email)
+    if User.objects.filter(email=new_email).exclude(id=user.id).exists():
+        return JsonResponse({"message": "Email already exists"})
+    
+    user.email = new_email
+    user.save() 
+
+    profile.phone=request.data.get("phone",profile.phone)
+    profile.save()
+    return JsonResponse({"message":"Update succesfully"})
+    
+# @api_view(['DELETE'])
+# @permisiion_classes([IsAuthenticated])
+# def delete_profile(request):
+
+
+
+
